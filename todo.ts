caa586ba8f513c80572565292707071b980afe41
zaryab khan <zaryab.2k1@gmail.com>
2023-10-31 22:27:02 +0500

    first commit

---
+++ todo.ts
@@ -0,0 +1,42 @@
+import inquirer from "inquirer"
+let todo:string[]=[];
+let choice="y";
+let option;
+while(choice=="y"){
+ option=await inquirer.prompt({
+name:"option",
+type:"input",
+message:"press 1 to add item to the list and 2 to show all items"
+
+
+ })
+
+ if(option.option==="1"){
+
+todo[todo.length]=(await inquirer.prompt({
+    name:"option",
+    type:"input",
+    message:"enterthe task"
+    
+    
+     })).option;
+
+ }
+
+ if(option.option==="2"){
+
+   todo.forEach((x)=>{
+console.log(x);
+
+   })
+    
+     }
+     choice=(await inquirer.prompt({
+        name:"choie",
+        type:"input",
+        message:"press y to perform anotger action ANN N TO EXIT"
+        
+        
+         })).choie;
+
+}
